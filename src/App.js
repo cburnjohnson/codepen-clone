56d25e47d14d05db8e6e2b95e1c8e7f9c28ff1cd
Cole Johnson <cburnjohnson@gmail.com>
2021-03-09 11:53:28 -0800

    initial editors

--- src/App.js
+++ src/App.js
@@ -1,6 +1,17 @@
+import Editor from './components/Editor'
+
 function App() {
   return (
-    <div>hello world</div>
+    <>
+      <div className='pane top-pane'>
+        <Editor />
+        <Editor />
+        <Editor />
+      </div>
+      <div className='pane'>
+        <iframe title='output' sandbox='allow-scripts' frameBorder='0' width='100%' height='100%'/>
+      </div>
+    </>
   );
 }
 

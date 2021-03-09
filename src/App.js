1e4c0f4397c49434584018721757c0b4a1999e15
Cole Johnson <cburnjohnson@gmail.com>
2021-03-09 12:07:48 -0800

    load html/css/js to pane

--- src/App.js
+++ src/App.js
@@ -1,10 +1,27 @@
-import React, {useState} from 'react'
+import React, {useState, useEffect} from 'react'
 import Editor from './components/Editor'
 
 function App() {
   const [html, setHtml] = useState('')
   const [css, setCss] = useState('')
   const [js, setJs] = useState('')
+  const [srcDoc, setSrcDoc] = useState('')
+
+  useEffect(() => {
+    const timeout = setTimeout(() => {
+      setSrcDoc(
+        `
+    <html>
+      <body>${html}</body>
+      <style>${css}</style>
+      <script>${js}</script>
+    </html>
+  `
+      )
+    }, 250)
+
+    return () => clearTimeout(timeout)
+  }, [html, css, js])
 
   return (
     <>
@@ -14,7 +31,7 @@ function App() {
         <Editor language='javascript' displayName='JS' value={js} onChange={setJs}/>
       </div>
       <div className='pane'>
-        <iframe title='output' sandbox='allow-scripts' frameBorder='0' width='100%' height='100%'/>
+        <iframe srcDoc={srcDoc} title='output' sandbox='allow-scripts' frameBorder='0' width='100%' height='100%'/>
       </div>
     </>
   );
